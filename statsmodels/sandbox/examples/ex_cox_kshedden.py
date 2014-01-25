b80aa434d3cf74b65c8df43dd38850ae738aead0
Skipper Seabold <jsseabold@gmail.com>
2014-01-25 11:01:59 -0500

    EX: Add some profiling code.

--- statsmodels/sandbox/examples/ex_cox_kshedden.py
+++ statsmodels/sandbox/examples/ex_cox_kshedden.py
@@ -17,6 +17,20 @@ mod_ss = CoxPH(dta.endog, dta.exog, ties='breslow')
 res_ss = mod_ss.fit()
 
 # assume you don't have a Survial object in dta.endog already
-surv = Survival(time1=data.data['time'], event=data['event'])
+surv = Survival(time1=dta.data['time'], event=dta.data['event'])
 mod_ss = CoxPH(surv, dta.exog, ties='breslow')
 res_ss = mod_ss.fit()
+
+# let's profile
+import cProfile, pstats
+params = res_ss.params
+cProfile.run("mod_ss.hessian(params)", "ss.prof")
+
+params = res.params
+cProfile.run("mod.hessian(params)", "ks.prof")
+
+p1 = pstats.Stats("ss.prof")
+p1.strip_dirs().sort_stats('time').print_stats()
+
+p2 = pstats.Stats("ks.prof")
+p2.strip_dirs().sort_stats('time').print_stats()
