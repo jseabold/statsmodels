bf73b2696f89aedca364a97eceb5da88f445355e
Skipper Seabold <jsseabold@gmail.com>
2015-07-12 18:24:24 -0500

    ENH: Update for notebook v4.

--- tools/nbgenerate.py
+++ tools/nbgenerate.py
@@ -11,10 +11,9 @@ OUTPUT = "html"
 import os
 import io
 import sys
-import time
-import shutil
-import platform
+import json
 import logging
+import platform
 from time import sleep
 
 SOURCE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..",
@@ -23,16 +22,12 @@ SOURCE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..",
 
 try:
     from Queue import Empty
-except ImportError:
+except ImportError:  # Python 3
     from queue import Empty
 
-try: # IPython has been refactored
-    from IPython.kernel import KernelManager
-except ImportError:
-    from IPython.zmq.blockingkernelmanager import (BlockingKernelManager as
-                                                   KernelManager)
+from IPython.kernel import KernelManager
 
-from IPython.nbformat.current import reads, write, NotebookNode
+from IPython.nbformat import reads, NotebookNode, convert
 
 cur_dir = os.path.abspath(os.path.dirname(__file__))
 
@@ -44,11 +39,15 @@ try:
 except ImportError:
     from warnings import warn
     from statsmodels.tools.sm_exceptions import ModuleUnavailableWarning
-    warn("Notebook examples not built. You need IPython 1.0.",
+    warn("Notebook examples not built. You need IPython >= 3.x.",
          ModuleUnavailableWarning)
     sys.exit(0)
 
 import hash_funcs
+
+
+class NotebookError(Exception):
+    pass
 
 
 class NotebookRunner(object):
@@ -66,18 +65,11 @@ class NotebookRunner(object):
         'image/svg+xml': 'svg',
     }
 
-
-    def __init__(self, nb, pylab=False, mpl_inline=False, profile_dir=None, working_dir=None):
+    def __init__(self, nb, mpl_inline=False, profile_dir=None,
+                 working_dir=None):
         self.km = KernelManager()
 
         args = []
-
-        if pylab:
-            args.append('--pylab=inline')
-            logging.warn('--pylab is deprecated and will be removed in a future version')
-        elif mpl_inline:
-            args.append('--matplotlib=inline')
-            logging.warn('--matplotlib is deprecated and will be removed in a future version')
 
         if profile_dir:
             args.append('--profile-dir=%s' % os.path.abspath(profile_dir))
@@ -87,7 +79,7 @@ class NotebookRunner(object):
         if working_dir:
             os.chdir(working_dir)
 
-        self.km.start_kernel(extra_arguments = args)
+        self.km.start_kernel(extra_arguments=args)
 
         os.chdir(cwd)
 
@@ -107,8 +99,10 @@ class NotebookRunner(object):
             # IPython < 3
             self._wait_for_ready_backport()
 
-        self.nb = nb
+        if mpl_inline:
+            self.kc.execute("%matplotlib inline")
 
+        self.nb = nb
 
     def shutdown_kernel(self):
         logging.info('Shutdown kernel')
@@ -135,8 +129,8 @@ class NotebookRunner(object):
         '''
         Run a notebook cell and update the output of that cell in-place.
         '''
-        logging.info('Running cell:\n%s\n', cell.input)
-        self.kc.execute(cell.input)
+        logging.info('Running cell:\n%s\n', cell.source)
+        self.kc.execute(cell.source)
         reply = self.kc.get_shell_msg()
         status = reply['content']['status']
         if status == 'error':
@@ -154,16 +148,16 @@ class NotebookRunner(object):
                     if msg['content']['execution_state'] == 'idle':
                         break
             except Empty:
-                # execution state should return to idle before the queue becomes empty,
-                # if it doesn't, something bad has happened
+                # execution state should return to idle before the queue
+                # becomes empty, if it doesn't, something bad has happened
                 raise
 
             content = msg['content']
             msg_type = msg['msg_type']
 
-            # IPython 3.0.0-dev writes pyerr/pyout in the notebook format but uses
-            # error/execute_result in the message spec. This does the translation
-            # needed for tests to pass with IPython 3.0.0-dev
+            # IPython 3.0.0-dev writes pyerr/pyout in the notebook format but
+            # uses error/execute_result in the message spec. This does the
+            # translation needed for tests to pass with IPython 3.0.0-dev
             notebook3_format_conversions = {
                 'error': 'pyerr',
                 'execute_result': 'pyout'
@@ -180,49 +174,49 @@ class NotebookRunner(object):
                 continue
             elif msg_type == 'stream':
                 out.stream = content['name']
+                out.name = content['name']  # for v4
                 # in msgspec 5, this is name, text
                 # in msgspec 4, this is name, data
                 if 'text' in content:
                     out.text = content['text']
                 else:
                     out.text = content['data']
-                #print(out.text, end='')
+                # print(out.text, end='')
             elif msg_type in ('display_data', 'pyout'):
                 for mime, data in content['data'].items():
                     try:
                         attr = self.MIME_MAP[mime]
                     except KeyError:
-                        raise NotImplementedError('unhandled mime type: %s' % mime)
+                        raise NotImplementedError('unhandled mime type: %s'
+                                                  % mime)
 
                     setattr(out, attr, data)
-                #print(data, end='')
+                # print(data, end='')
             elif msg_type == 'pyerr':
                 out.ename = content['ename']
                 out.evalue = content['evalue']
                 out.traceback = content['traceback']
 
-                #logging.error('\n'.join(content['traceback']))
+                # logging.error('\n'.join(content['traceback']))
             elif msg_type == 'clear_output':
                 outs = list()
                 continue
             else:
-                raise NotImplementedError('unhandled iopub message: %s' % msg_type)
+                raise NotImplementedError('unhandled iopub message: %s' %
+                                          msg_type)
             outs.append(out)
         cell['outputs'] = outs
 
         if status == 'error':
             raise NotebookError(traceback_text)
 
-
     def iter_code_cells(self):
         '''
         Iterate over the notebook cells containing code.
         '''
-        for ws in self.nb.worksheets:
-            for cell in ws.cells:
-                if cell.cell_type == 'code':
-                    yield cell
-
+        for cell in self.nb.cells:
+            if cell.cell_type == 'code':
+                yield cell
 
     def run_notebook(self, skip_exceptions=False, progress_callback=None):
         '''
@@ -241,7 +235,6 @@ class NotebookRunner(object):
             if progress_callback:
                 progress_callback(i)
 
-
     def count_code_cells(self):
         '''
         Return the number of code cells in the notebook
@@ -255,7 +248,8 @@ def _get_parser():
     except ImportError:
         raise ImportError("This script only runs on Python >= 2.7")
     parser = argparse.ArgumentParser(description="Convert .ipynb notebook "
-                                      "inputs to HTML page with output")
+                                                 "inputs to HTML page with "
+                                                 "output")
     parser.add_argument("path", type=str, default=SOURCE_DIR, nargs="?",
                         help="path to folder containing notebooks")
     parser.add_argument("--profile", type=str,
@@ -264,6 +258,7 @@ def _get_parser():
                         metavar="N",
                         help="how long to wait for cells to run in seconds")
     return parser
+
 
 def nb2html(nb):
     """
@@ -277,75 +272,48 @@ def nb2html(nb):
     C = HTMLExporter(config=config)
     return C.from_notebook_node(nb)[0]
 
-def nb2rst(nb, files_dir):
-    """
-    nb should be a NotebookNode
-    """
-    #NOTE: This does not currently work. Needs to be update to IPython 1.0.
-    config = Config()
-    C = ConverterRST(config=config)
-    # bastardize how this is supposed to be called
-    # either the API is broken, or I'm not using it right
-    # why can't I set this using the config?
-    C.files_dir = files_dir + "_files"
-    if not os.path.exists(C.files_dir):
-        os.makedirs(C.files_dir)
-    # already parsed into a NotebookNode
-    C.nb = nb
-    return C.convert()
 
 if __name__ == '__main__':
     rst_target_dir = os.path.join(cur_dir, '..',
-                        'docs/source/examples/notebooks/generated/')
+                                  'docs/source/examples/notebooks/generated/')
     if not os.path.exists(rst_target_dir):
         os.makedirs(rst_target_dir)
 
-    parser = _get_parser()
-    arg_ns, other_args = parser.parse_known_args()
+    is_nb = lambda x : x.endswith('.ipynb') and 'generated' not in x
+    contents = os.listdir(SOURCE_DIR)
+    notebooks = [os.path.join(SOURCE_DIR, i) for i in filter(is_nb, contents)]
 
-    os.chdir(arg_ns.path) # so we execute in notebook dir
-    notebook_runner = NotebookRunner(arg_ns.path, other_args, arg_ns.profile,
-                                     arg_ns.timeout)
     try:
-        for fname, nb in notebook_runner:
+        for fname in notebooks:
+            f = open(fname, 'r').read()
             base, ext = os.path.splitext(fname)
             fname_only = os.path.basename(base)
             # check if we need to write
-            towrite, filehash = hash_funcs.check_hash(open(fname, "r").read(),
-                                                      fname_only)
+            towrite, filehash = hash_funcs.check_hash(f, fname_only)
             if not towrite:
                 print("Hash has not changed for file %s" % fname_only)
                 continue
             print("Writing ", fname_only)
+            nb = json.loads(f)
+            nb_version = nb['nbformat']
+            nb = reads(f, as_version=nb_version)
+            if nb_version != 4:
+                nb = convert(nb, 4)
+            notebook_runner = NotebookRunner(nb, working_dir=SOURCE_DIR,
+                                             profile_dir=None,
+                                             mpl_inline=True)
 
             # This edits the notebook cells inplace
-            notebook_runner(nb)
-            # for debugging writes ipynb file with output
-            #new_ipynb = "%s_generated%s" % (base, ".ipynb")
-            #with io.open(new_ipynb, "w", encoding="utf-8") as f:
-            #    write(nb, f, "json")
+            notebook_runner.run_notebook()
 
             # use nbconvert to convert to rst
             support_file_dir = os.path.join(rst_target_dir,
                                             fname_only+"_files")
-            if OUTPUT == "rst":
-                new_rst = os.path.join(rst_target_dir, fname_only+".rst")
-                rst_out = nb2rst(nb, fname_only)
-                # write them to source directory
-                if not os.path.exists(rst_target_dir):
-                    os.makedirs(rst_target_dir)
-                with io.open(new_rst, "w", encoding="utf-8") as f:
-                    f.write(rst_out)
-
-                # move support files
-                if os.path.exists(fname_only+"_files"):
-                    shutil.move(fname_only+"_files",
-                            os.path.join(rst_target_dir, fname_only+"_files"))
-            elif OUTPUT == "html":
+            if OUTPUT == "html":
                 from notebook_output_template import notebook_template
                 new_html = os.path.join(rst_target_dir, fname_only+".rst")
                 # get the title out of the notebook because sphinx needs it
-                title_cell = nb['worksheets'][0]['cells'].pop(0)
+                title_cell = nb['cells'].pop(0)
                 if title_cell['cell_type'] == 'heading':
                     pass
                 elif (title_cell['cell_type'] == 'markdown'
