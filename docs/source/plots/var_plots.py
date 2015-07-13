d498cc30c9872b0f89265e4db2a3f12e8540f9f0
Skipper Seabold <jsseabold@gmail.com>
2015-07-12 21:50:54 -0500

    COMPAT: Python 3

--- docs/source/plots/var_plots.py
+++ docs/source/plots/var_plots.py
@@ -9,7 +9,7 @@ import pandas
 mdata = ds.macrodata.load_pandas().data
 
 # prepare the dates index
-dates = mdata[['year', 'quarter']].astype(int).astype('S4')
+dates = mdata[['year', 'quarter']].astype(int).astype(str)
 quarterly = dates["year"] + "Q" + dates["quarter"]
 quarterly = dates_from_str(quarterly)
 
